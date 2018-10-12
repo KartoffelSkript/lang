ffd035495742f3da53766377c265704686a235a2
Merlin O <merlinosayimwen@gmail.com>
2018-10-12 20:35:30 +0200

    Add some initial source-code files.

---
+++ src/kscript.h
@@ -0,0 +1,15 @@
+// Copyright 2018 the Kartoffelskript project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef KSCRIPT_KSCRIPT_H
+#define KSCRIPT_KSCRIPT_H
+
+#include <iostream>
+
+namespace kscript {
+
+
+}
+
+#endif //KSCRIPT_KSCRIPT_H
