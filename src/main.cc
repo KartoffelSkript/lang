da306261c250eed25f9daa9f8ede8affe0b8c26b
Merlin O <merlinosayimwen@gmail.com>
2018-10-13 15:20:56 +0200

    Let the main.cc file use the logger for prints.

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,13 @@
 // found in the LICENSE file.
 
 #include "kscript.h"
+#include "log.h"
 
 int main(int argc, char **argv) {
-  std::cout << "Hallo kartoffelskript" << std::endl;
+  using namespace kscript;
+
+  StreamStringWriter writer(std::cout);
+  Logger logger(&writer, "", Logger::Level::kDebug);
+
+  logger.Log(Logger::Level::kDebug, "Hello Kartoffelkript!");
 }
