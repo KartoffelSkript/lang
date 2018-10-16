fc2ee3e5bd41b3b339696d29e6d287987e0c3e79
Merlin O <merlinosayimwen@gmail.com>
2018-10-16 21:13:11 +0200

    Remove parts of old logger library from main method.

--- src/main.cc
+++ src/main.cc
@@ -2,14 +2,5 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "kscript.h"
-#include "log.h"
-
 int main(int argc, char **argv) {
-  using namespace kscript;
-
-  StreamStringWriter writer(std::cout);
-  Logger logger(&writer, "", Logger::Level::kDebug);
-
-  logger.Log(Logger::Level::kDebug, "Hello Kartoffelkript!");
 }
